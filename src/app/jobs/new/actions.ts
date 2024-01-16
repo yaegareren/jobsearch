74a46b5db9b09a52a4a80ad2617afbb800a367fb
Shree Mangalam <sshreemangalam@gmail.com>
2024-01-16 18:31:09 +0530

    minor fix

--- src/app/jobs/new/actions.ts
+++ src/app/jobs/new/actions.ts
@@ -54,7 +54,6 @@ export async function CreateJobPosting(formData: FormData) {
       applicationUrl: applicationUrl?.trim(),
       description: description?.trim(),
       salary: parseInt(salary),
-      approved: true,
     },
   });
   redirect("/job-submitted");
