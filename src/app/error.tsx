76124a7e48791b27aea7dce3025b3a4471338dcf
Shree Mangalam <sshreemangalam@gmail.com>
2024-01-17 23:31:53 +0530

    admin code

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -4,9 +4,9 @@ import H1 from "@/components/ui/h1";
 
 export default function error() {
   return (
-    <main className="m-auto max-w-5xl my-10 space-y-5 px-3 text-center ">
+    <main className="m-auto my-10 max-w-5xl space-y-5 px-3 text-center ">
       <H1>Error</H1>
-      <p>An unexpected error occurred!</p>
+      <p>An unexpected error occurred.</p>
     </main>
   );
 }
